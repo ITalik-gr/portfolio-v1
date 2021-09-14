ebab3baecc65c690be5049d2fdd0d4b87c883f61
Italik <italik.gr@gmail.com>
2021-09-14 19:11:34 +0300

    add discuss-btn animation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,11 @@
 
 
 let nav_toggle = document.querySelector('.nav-toggle'),
-    menu = document.querySelector('.menu');
+  menu = document.querySelector('.menu');
 
 console.log(nav_toggle);
 
 nav_toggle.addEventListener('click', function() {
-  nav_toggle.classList.toggle('nav-toggle-active')
-  menu.classList.toggle('menu-active')
+nav_toggle.classList.toggle('nav-toggle-active')
+menu.classList.toggle('menu-active')
 });
