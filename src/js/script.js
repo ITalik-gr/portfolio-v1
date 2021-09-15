3803f188d6b96d8b2baad069e2740b68d09994ff
Italik <italik.gr@gmail.com>
2021-09-15 14:57:23 +0300

    add animation preloader + timer % script

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,9 @@
 
+window.onload = function () {
+  let ab = setTimeout(function() {
+    document.querySelector('.preloader').classList.add('preloader-none');
+  }, 2800)
+}
 
 let nav_toggle = document.querySelector('.nav-toggle'),
   menu = document.querySelector('.menu');
