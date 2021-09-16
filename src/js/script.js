8f579912a2dcfa381d26c85f2e369be2a6e31715
Italik <italik.gr@gmail.com>
2021-09-16 17:09:01 +0300

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 
+
 let text1 = document.querySelector('.header-name'),
     text2 = document.querySelector('.header-title'),
     text3 = document.querySelector('.header-subtitle'),
@@ -13,9 +14,9 @@ let text1 = document.querySelector('.header-name'),
       nav.classList.add('opacity-animate')
     }
 window.onload = function () {
-  // let ab = setTimeout(function() {
-  //   document.querySelector('.preloader').classList.add('preloader-none');
-  // }, 2800)
+  let ab = setTimeout(function() {
+    document.querySelector('.preloader').classList.add('preloader-none');
+  }, 2800)
   let aa = setTimeout(function() {
     text_anim();
     opacity();
@@ -35,3 +36,29 @@ nav_toggle.addEventListener('click', function() {
 
 
 console.log('Frontend developing by italik');
+
+
+//!
+
+$(document).ready(function(){
+  $("#follower").hover(function(){
+    var mouseX = 0, mouseY = 0;
+$(document).mousemove(function(e){
+   mouseX = e.pageX;
+   mouseY = e.pageY; 
+});
+
+// cache the selector
+var follower = $("#follower");
+var xp = 0, yp = 0;
+var loop = setInterval(function(){
+    // change 12 to alter damping higher is slower
+    xp += (mouseX - xp) / 12;
+    yp += (mouseY - yp) / 12;
+    follower.css({left:xp, top:yp});
+    
+}, 30);
+    }, function(){
+      $(this).off('mousemove').css("background-color", "pink");
+  });
+});
